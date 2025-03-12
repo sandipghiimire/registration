0e21a4efc7ae70e4072ddf8e70aa58928c981427
Sandip <sandipghimire254@gmail.com>
2025-03-12 11:28:15 +0545

    Deleting

--- backend/routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const {register, login} = require('../controllers/AuthController');
-const {signupValidation, loginValidation} = require('../Middlewares/AuthValidation');
-
-const router = require('express').Router();
-
-// router.post('/login', loginValidation, login);
-router.post('/register', signupValidation, register);
-
-module.exports = router;
